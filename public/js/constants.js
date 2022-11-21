a8c79589f5fa9fbe7e08412656ee815a4a94cd4f
Felipe Galindo <galindoaguilarf@gmail.com>
2022-11-21 03:16:06 -0600

    feat: git add url heroku server

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,4 +1,4 @@
-const hots = "http://localhost:8081";
+const hots = "https://personal-app-ga.herokuapp.com";
 const findByPageEndPoint = "/api/v1/pokemon/page/{page}/limit/{limit}";
 const findEvolutionsEndPoint= "/api/v1/pokemon/envolve/{idPokemon}";
 const limit = 20;
